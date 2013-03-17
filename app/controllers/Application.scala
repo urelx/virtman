a8ada7f24e99fd1a40ef2615d988da005b1ba516
Yamanaka Yuu <yuu@relx.jp>
2013-03-18 01:43:10 +0900

    VM shutdown/startup request via Ajax

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,29 @@ package controllers
 import play.api._
 import play.api.mvc._
 import models.VM
+import views.html.defaultpages.badRequest
+import models.VMState
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index(VM.list()))
+  }
+  
+  def changeVMState(vmID: Int) = Action(parse.json) { implicit request =>
+    Logger.debug("PUT /vm/%d" format vmID)
+    (request.body \ "state").asOpt[String] match {
+      case None => BadRequest("state param required.")
+      case Some("stopped") => { VM.shutdown(vmID); Ok }
+      case Some("running") => { VM.startup(vmID); Ok }
+      case Some(other) => 
+        BadRequest("illegal state value. [%s]" format other)
+    }
+  }
+  
+  def javascriptRoutes = Action { implicit request =>
+    Ok(Routes.javascriptRouter("jsRouter", Some("jQuery.ajax"))(
+      routes.javascript.Application.changeVMState)).as("text/javascript")
   }
   
 }
--- app/models/VM.scala
+++ app/models/VM.scala
@@ -1,5 +1,7 @@
 package models
 
+import play.Logger
+
 /**
  * State of VM
  */
@@ -32,5 +34,25 @@ object VM {
     VM(1, "hoge", Running),
     VM(2, "huga", Stopped),
     VM(3, "piyo", Unknown))
+  
+  /**
+   * Send shutdown signal to specified VM
+   * 
+   * @param vmID VM ID 
+   */
+  def shutdown(vmID: Int) {
+    // TODO
+    Logger.debug("shutdown request received. [vmID=%s]" format vmID)
+  }
+    
+  /**
+   * Start Up specified VM
+   * 
+   * @param vmID VM ID
+   */
+  def startup(vmID: Int) {
+    // TODO
+    Logger.debug("start request received. [vmID=%s]" format vmID)
+  }
       
 }
